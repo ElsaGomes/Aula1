d76c481269cc167e207c5e4a3626080824ab36f3
ElsaGomes <elsaandreina13@gmail.com>
2022-01-04 15:59:38 +0000

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,27 +1,15 @@
 Class teste
 {
-      public static void main(String args[])
-  {
-    Scanner consola = new Scanner(System.in);
-
-    System.out.println("Escreva o seu ano de nascimento: ");
-    int anoNascimento = consola.nextInt();
-
-
-
-    int idade = calculaIdade(anoNascimento);
-
-
-    System.out.println("A sua idade é: " + idade);
-
-    consola.close();
-
-  }
-
-  public static int calculaIdade(int aAnoNascimento)
-  {
-    int anoAtual = Calendar.getInstance().get(Calendar.YEAR); 
-
-    return anoAtual - aAnoNascimento;
-  }
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in); 
+		
+		System.out.println("Escreva o seu nome:");
+		String nome = in.nextLine();
+		
+		System.out.println("O meu nome é " + nome);	
+		
+		in.close();
+	 }
+}
 }
